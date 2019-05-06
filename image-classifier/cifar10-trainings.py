921ca10d87ffd09250e66466b591d796ba16a254
mrcabo <dcgdiego@gmail.com>
2019-05-06 11:11:03 +0200

    Learning rate scheduler and few mods. now resnet sortof works well

--- image-classifier/cifar10-trainings.py
+++ image-classifier/cifar10-trainings.py
@@ -1,5 +1,7 @@
 from keras import applications
 from keras.optimizers import Adam, SGD
+from keras.callbacks import LearningRateScheduler
+from keras.callbacks import ReduceLROnPlateau
 from keras.preprocessing.image import ImageDataGenerator
 from keras import utils
 from keras.datasets import cifar10
@@ -32,6 +34,31 @@ def printGraph(history):
     plt.savefig('model-loss.png')
 
 
+def lr_schedule(epoch):
+    """Learning Rate Schedule
+
+    Learning rate is scheduled to be reduced after 80, 120, 160, 180 epochs.
+    Called automatically every epoch as part of callbacks during training.
+
+    # Arguments
+        epoch (int): The number of epochs
+
+    # Returns
+        lr (float32): learning rate
+    """
+    lr = 1e-3
+    if epoch > 90:
+        lr *= 0.5e-3
+    elif epoch > 80:
+        lr *= 1e-3
+    elif epoch > 60:
+        lr *= 1e-2
+    elif epoch > 40:
+        lr *= 1e-1
+    print('Learning rate: ', lr)
+    return lr
+
+
 def build_model(input_shape, pre_trained_weights, num_classes, dropout=False, model='resnet'):
     if model == 'densenet121':
         base_model = applications.DenseNet121(weights=pre_trained_weights, include_top=False, input_shape=input_shape)
@@ -39,7 +66,6 @@ def build_model(input_shape, pre_trained_weights, num_classes, dropout=False, mo
         base_model = applications.VGG16(weights=pre_trained_weights, include_top=False, input_shape=input_shape)
     else:
         base_model = applications.ResNet50(weights=pre_trained_weights, include_top=False, input_shape=input_shape)
-
     if pre_trained_weights == 'imagenet':
         for layer in base_model.layers:
             layer.trainable = False
@@ -48,7 +74,7 @@ def build_model(input_shape, pre_trained_weights, num_classes, dropout=False, mo
     if dropout:
         x = Dropout(0.7)(x)
     # let's add a fully-connected layer
-    # x = Dense(1024, activation='relu')(x)
+    x = Dense(1024, activation='relu')(x)
     # and a logistic layer -- let's say we have 200 classes
     predictions = Dense(num_classes, activation='softmax')(x)
     # this is the model we will train
@@ -61,9 +87,11 @@ def run_model(x_train_data, y_train_data, x_test_data, y_test_data, model_name, 
               dropout=False):
     model = build_model(input_shape, pre_trained_weights, num_classes, dropout, model=model_name)
     if optimizer == 'SGD':
-        opt = SGD(lr=0.001, momentum=0.9, decay=1e-3, nesterov=True)
+        opt = SGD(lr=lr_schedule(0))
+    elif optimizer == 'SGD_nesterov':
+        opt = SGD(lr=lr_schedule(0), momentum=0.9, decay=1e-3, nesterov=True)
     else:
-        opt = Adam(lr=0.0001)
+        opt = Adam(lr=lr_schedule(0))
 
     if loss_typename == 'mse':
         loss = 'mse'
@@ -71,19 +99,30 @@ def run_model(x_train_data, y_train_data, x_test_data, y_test_data, model_name, 
         loss = 'categorical_crossentropy'
 
     model.compile(optimizer=opt, loss=loss, metrics=['accuracy'])
-
+    model.summary()
     with_gpu = K.tensorflow_backend._get_available_gpus()
     print('If not empty, the code is using GPU:')
     print(with_gpu)
 
-    history = model.fit(x_train_data, y_train_data, epochs=epochs, batch_size=batch_size,
-                        validation_data=(x_test_data, y_test_data))
+    lr_scheduler = LearningRateScheduler(lr_schedule)
+    lr_reducer = ReduceLROnPlateau(factor=np.sqrt(0.1),
+                                   cooldown=0,
+                                   patience=5,
+                                   min_lr=0.5e-6)
+
+    callbacks = [lr_reducer, lr_scheduler]
+    history = model.fit(x_train_data, y_train_data,
+                        epochs=epochs,
+                        batch_size=batch_size,
+                        validation_data=(x_test_data, y_test_data),
+                        shuffle=True,
+                        callbacks=callbacks)
     printGraph(history)
 
-    preds = model.evaluate(x_test_data, y_test_data)
-    print("Loss = " + str(preds[0]))
-    print("Test Accuracy = " + str(preds[1]))
-    model.summary()
+    # Score trained model.
+    scores = model.evaluate(x_test_data, y_test_data, verbose=1)
+    print('Test loss:', scores[0])
+    print('Test accuracy:', scores[1])
 
 
 def print_params(model_name, batch_size, epochs, optimizer, loss, dropout, data_augmentation, pre_trained):
@@ -99,11 +138,13 @@ def print_params(model_name, batch_size, epochs, optimizer, loss, dropout, data_
 
 # Training parameters
 
+model_name = 'resnet'
 # model_name = 'densenet121'
-model_name = 'vgg16'
-# model_name = 'resnet'
-batch_size = 64 # For densenet - https://arxiv.org/pdf/1608.06993.pdf
-epochs = 80
+# model_name = 'vgg16'
+# batch_size = 64 # For densenet - https://arxiv.org/pdf/1608.06993.pdf
+# epochs = 80
+batch_size = 64
+epochs = 200
 # optimizer = 'SGD'
 optimizer = 'Adam'
 loss = 'categorical_crossentropy'
